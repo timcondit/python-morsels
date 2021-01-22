42d7f26ed4ad1b8f25e5c80d8a30a31408c56803
Tim Condit <tcondit@gmail.com>
2021-01-21 20:44:06 -0800

    Implement count

---
+++ count.py
@@ -0,0 +1,34 @@
+# count.py
+#
+# I want you to write a function that accepts a string and returns a mapping (a
+# dictionary or dictionary-like structure) that has words as the keys and the
+# number of times each word was seen as the values.
+#
+# Your function should work like this:
+#
+# >>> count_words("oh what a day what a lovely day")
+# {'oh': 1, 'what': 2, 'a': 2, 'day': 2, 'lovely': 1}
+# >>> count_words("don't stop believing")
+# {"don't": 1, 'stop': 1, 'believing': 1}
+
+import re
+
+def count_words(words):
+    regex = r'\w+'
+    wordslist = re.findall(regex, words)
+    #wordslist = words.split(" ")
+    wordsdict = {}
+    for word in wordslist:
+        word = word.lower()
+        if word in wordsdict:
+            wordsdict[word] += 1
+        else:
+            wordsdict[word] = 1
+    return wordsdict
+
+
+if __name__ == "__main__":
+    count_words("oh what a day what a lovely day")
+    count_words("Oh what a day what a lovely day")
+    count_words("Oh what a day, what a lovely day!")
+    count_words("don't stop believing")
